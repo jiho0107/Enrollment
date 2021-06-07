e8819e3cc579b3c7b20082821f38bcdf2292a2e4
jiho0107 <81346282+jiho0107@users.noreply.github.com>
2021-06-07 11:32:25 +0900

    Add files via upload

---
+++ test0601/src/enroll/controller/StuInfoController.java
@@ -0,0 +1,28 @@
+package enroll.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import enroll.service.Service;
+import enroll.vo.StudentVO;
+
+public class StuInfoController implements Controller {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id = (String)request.getSession().getAttribute("id"); //교수id
+		String subId = request.getParameter("subId"); //과목id
+		ArrayList<StudentVO> list = Service.getInstance().stuInfo(id,subId);
+		request.setAttribute("list", list);
+		String subName = request.getParameter("subName");//과목명
+		request.setAttribute("subName", subName);
+		
+		HttpUtil.forward(request, response, "/result/stuInfoResult.jsp");
+	}
+
+}
